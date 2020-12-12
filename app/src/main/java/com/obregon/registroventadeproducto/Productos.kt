4c1f474b65927708d53f99f708b08ed8172ecec3
Matilde24 <matyarostegui@gmail.com>
2020-12-12 13:20:46 -0600

    Agregué paquetes en la android

---
+++ app/src/main/java/com/obregon/registroventadeproducto/Productos.kt
@@ -0,0 +1,11 @@
+package com.obregon.registroventadeproducto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Productos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_productos)
+    }
+}

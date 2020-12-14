adf7097905c517e0b8d18c836893e87b33cd24ce
Matilde24 <matyarostegui@gmail.com>
2020-12-14 16:25:11 -0600

    MainActivity

--- app/src/main/java/com/obregon/registroventadeproducto/MainActivity.kt
+++ app/src/main/java/com/obregon/registroventadeproducto/MainActivity.kt
@@ -1,4 +1,27 @@
 package com.obregon.registroventadeproducto
 
-class MainActivity {
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.properties.Delegates
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun lanzarClima(view: View) {
+
+        val intent = Intent(this, Clima::class.java)
+        startActivity(intent)
+    }
 }
